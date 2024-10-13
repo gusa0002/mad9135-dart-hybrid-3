14fdad2fce9c01a0d2a0785068b154ca2bbdb154
Anton <gusa0002@algonquinlive.com>
2024-10-13 16:35:24 -0400

    Init project

---
+++ bin/mad9135_dart_hybrid_3.dart
@@ -0,0 +1,5 @@
+import 'package:mad9135_dart_hybrid_3/mad9135_dart_hybrid_3.dart' as mad9135_dart_hybrid_3;
+
+void main(List<String> arguments) {
+  print('Hello world: ${mad9135_dart_hybrid_3.calculate()}!');
+}
---
+++ test/mad9135_dart_hybrid_3_test.dart
@@ -0,0 +1,8 @@
+import 'package:mad9135_dart_hybrid_3/mad9135_dart_hybrid_3.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
